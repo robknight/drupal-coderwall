a5667010cb79b921caf64f33c861ab12624663e9
Rob Knight <mail@robknight.org.uk>
2012-06-11 23:36:37 +0100

    Initial commit

---
+++ templates/coderwall_achievements.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file Coderwall achievements template
+ * @see http://coderwall.com/api#blogbadge
+ */
+?>
+<section
+   class="coderwall"
+   data-coderwall-username="<?php echo $variables['element']['#username']; ?>"
+   data-coderwall-orientation="<?php echo $variables['element']['#orientation']; ?>">
+</section>
